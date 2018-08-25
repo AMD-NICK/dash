83036ad8f3bde0e3227cae78e1a2a26522dfb00b
Mad Kernel <amd@default.im>
2018-08-25 21:24:57 +0300

    util.lua
    override IsValid
    update file

--- lua/dash/extensions/util.lua
+++ lua/dash/extensions/util.lua
@@ -28,7 +28,7 @@ function util.Tracer(vecStart, vecEnd, pEntity, iAttachment, flVelocity, bWhiz, 
 
 	if (iAttachment ~= TRACER_DONT_USE_ATTACHMENT) then
 		fFlags = bit.bor(fFlags, TRACER_FLAG_USEATTACHMENT)
-		data:SetAttachment(iAttachment )
+		data:SetAttachment(iAttachment)
 	end
 
 	data:SetFlags(fFlags)
@@ -56,58 +56,64 @@ local badpoints = {
 	[CONTENTS_MONSTERCLIP] 	= true,
 }
 
-local function isempty(pos, area)
+function util.PosIsEmpty(pos, area, fAllowed)
 	if badpoints[util_PointContents(pos)] then
 		return false
 	end
+
 	local entities = ents_FindInSphere(pos, area)
 	for i = 1, #entities do
-		if (entities[i]:GetClass() == 'prop_physics') or (entities[i]:IsPlayer() and entities[i]:Alive()) then
+		if fAllowed and fAllowed(entities[i]) then continue end
+
+		if isProp(entities[i]) or (entities[i]:IsPlayer() and entities[i]:Alive()) then
 			return false
 		end
 	end
+
 	return true
 end
 
+local isEmpty = util.PosIsEmpty
 function util.FindEmptyPos(pos, area, steps)
 	pos = Vector(pos.x, pos.y, pos.z)
 	area = area or 35
 
-	if isempty(pos, area) then
+	if isEmpty(pos, area) then
 		return pos
 	end
 
 	for i = 1, (steps or 6) do
-		local step = (i * 50)
-		if isempty(Vector(pos.x + step, pos.y, pos.z), area) then
+		local step = i * math.random(20,50)
+		if isEmpty(Vector(pos.x + step, pos.y, pos.z), area) then
 			pos.x = pos.x + step
 			return pos
-		elseif isempty(Vector(pos.x, pos.y + step, pos.z), area) then
+		elseif isEmpty(Vector(pos.x, pos.y + step, pos.z), area) then
 			pos.y = pos.y + step
 			return pos
-		elseif isempty(Vector(pos.x, pos.y, pos.z + step), area) then
+		elseif isEmpty(Vector(pos.x, pos.y, pos.z + step), area) then
 			pos.z = pos.z + step
 			return pos
 		end
 	end
-	
+
 	return pos
 end
 
+
 local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 function util.Base64Decode(data)
-    data = string.gsub(data, '[^'..b..'=]', '')
-    return (data:gsub('.', function(x)
-        if (x == '=') then return '' end
-        local r,f='',(b:find(x)-1)
-        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
-        return r;
-    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
-        if (#x ~= 8) then return '' end
-        local c=0
-        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
-        return string.char(c)
-    end))
+	data = string.gsub(data, '[^'..b..'=]', '')
+	return (data:gsub('.', function(x)
+		if (x == '=') then return '' end
+		local r,f='',(b:find(x)-1)
+		for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+		return r;
+	end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+		if (#x ~= 8) then return '' end
+		local c=0
+		for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+		return string.char(c)
+	end))
 end
 
 function resource.AddDir(dir, recursive)
@@ -117,8 +123,18 @@ function resource.AddDir(dir, recursive)
 		resource.AddFile(dir .. v)
 	end
 	if (recursive == true) then
-		for k, v in ipairs(folders) do
-			resource.AddDir(dir .. v, recursive)
+		for _,d in ipairs(folders) do
+			if d == '.svn' then continue end
+			resource.AddDir(dir .. d, recursive)
 		end
 	end
+end
+
+function IsValid(object)
+	if (not object) then return false end
+
+	local isvalid = object.IsValid
+	if (not isvalid) then return false end
+
+	return isvalid(object)
 end
